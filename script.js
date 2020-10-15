22c934f6772a49f7b8970d4f1f8b5807496312fa
Dani JM <xdanielmtz@gmail.com>
2020-10-15 16:47:18 -0400

    now able to grab events by city using the API. only console logged now .

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 
-var ApiKey = "h2gbHeoXuSGHYUHwer9Jy6S7mT5Sj8oP"
-var QueryURL = "https://app.ticketmaster.com/discovery/v2/events.json?city=atlanta&apikey=" + ApiKey
-$.ajax({
-    url: QueryURL,
-    method: "GET"
-}).then(function(response){
-    console.log(response)
-})
+
+
+
 
 function cityName(){
-    $("#searchBtn").click(function(event){
+    $("#select-city").click(function(event){
         event.preventDefault()
+        var theCity = $("#user-destination").val();
+        var APIKey = "h2gbHeoXuSGHYUHwer9Jy6S7mT5Sj8oP";
+        var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?city=" + theCity + "&apikey=" + APIKey
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response){
+            console.log(response)
+        })
+        
     }
-
+    
 
 
 
 )}
+
+cityName();
