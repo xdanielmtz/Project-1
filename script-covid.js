5a316013ea6c85d2d0216a74ffe1407f40e89407
Arpita Saha <arpita.sahaa@gmail.com>
2020-10-15 11:12:38 -0400

    cleaned the code

--- script-covid.js
+++ script-covid.js
@@ -1,50 +1,40 @@
+function getCovidDetails(cityName) {
+  var geoURL =
+    "http://ec2-54-147-141-158.compute-1.amazonaws.com/api/cities?city=" +
+    cityName;
 
-var geoURL = "https://api.geocod.io/v1.6/geocode?q=atlanta&api_key=a3b545f33b7513f1aa5a508a505f7ba0f7f88f1";
-
-$.ajax({
+  $.ajax({
     url: geoURL,
     method: "GET",
-  }).then(function(response){
+  }).then(function (response) {
     console.log(response);
 
-    var stateCode = response.results[0].address_components.state;
+    var stateName = response.state;
 
-    var county = response.results[0].address_components.county;
-    var countyName = (county.split(" ")[0]);
+    var county = response.county;
 
-    //get state name using state code
-    var stateName = getStateName(stateCode);
-    console.log(stateName);
-
-    var diseaseAPi = "https://disease.sh/v3/covid-19/nyt/counties/"+countyName+"?lastdays=5";
-  
-    $.ajax({
-        url: diseaseAPi,
-        method: "GET",
-      }).then(function(response){
-          console.log(response);
-        for(i=0;i<response.length;i++){
-            if(stateName===response[i].state){
-                console.log(response[i]);
-
-            }
-        }
-      })
-    
+    getCovidDataForCounty(stateName, county);
   });
+}
 
-  function getStateName(stateCode){
-    for(i=0; i<stateArray.length;i++){
-        if(stateCode===stateArray[i].stateCode){
-            return stateArray[i].stateName;
-            
-        }
+function getCovidDataForCounty(stateName, county) {
+  // var diseaseAPi =
+  //   "https://disease.sh/v3/covid-19/nyt/counties/" + county + "?lastdays=5";
+  var diseaseAPi =
+    "https://disease.sh/v3/covid-19/nyt/counties/" + county + "?lastdays=1";
+  $.ajax({
+    url: diseaseAPi,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+    for (i = 0; i < response.length; i++) {
+      if (stateName === response[i].state) {
+        console.log(response[i]);
+        console.log("total cases " + response[i].cases+" , total deaths "+ response[i].deaths);
 
+        // displayCovidData();
+      }
     }
-
-  }
- 
-
-
-  
-
+  });
+}
+getCovidDetails("atlanta");
